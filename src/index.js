969038e3be02b6880d7c8b1e643213bf61634a25
Shantanu Sharma <shantanusharma6079@gmail.com>
2022-09-25 13:46:31 +0530

    reactupdate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ 
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
